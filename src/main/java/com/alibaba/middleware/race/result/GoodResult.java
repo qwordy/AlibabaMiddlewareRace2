78cbe6969bab0c215cab94fcab26f9ebdd131652
qwordy <iamyfy@163.com>
2016-07-22 11:48:04 +0800

    qwordy

--- src/main/java/com/alibaba/middleware/race/result/GoodResult.java
+++ src/main/java/com/alibaba/middleware/race/result/GoodResult.java
@@ -1,5 +1,6 @@
 package com.alibaba.middleware.race.result;
 
+import com.alibaba.middleware.race.HashTable;
 import com.alibaba.middleware.race.OrderSystem;
 import com.alibaba.middleware.race.Tuple;
 
@@ -21,28 +22,84 @@ public class GoodResult extends AbstractResult implements OrderSystem.Result {
       throws Exception {
 
     goodResultMap = goodResult.getResultMap();
+    int goodResultMapSize = goodResultMap.size();
+    if (goodResultMap.containsKey("goodid"))
+      goodResultMapSize--;
+
     this.keys = keys;
+    int targetSize = 0;
+    if (keys != null) {
+      targetSize = keys.size();
+      if (!keys.contains("orderid"))
+        targetSize++;
+    }
+
     resultMap = new HashMap<>();
     scan(orderTuple, resultMap);
+    if (keys == null || resultMap.size() + goodResultMapSize < targetSize) {
+      OrderSystem.KeyValue buyerKv = resultMap.get("buyerid");
+      if (buyerKv != null) {
+        Tuple buyerTuple = HashTable.buyerHashTable.get(buyerKv.valueAsString().getBytes());
+        scan(buyerTuple, resultMap);
+      }
+    }
   }
 
   @Override
   public OrderSystem.KeyValue get(String key) {
-    return null;
+    if (key.equals("orderid")) {
+      if (keys.contains(key))
+        return resultMap.get(key);
+      return null;
+    }
+
+    OrderSystem.KeyValue kv = resultMap.get(key);
+    if (kv != null)
+      return kv;
+
+    kv = goodResultMap.get(key);
+    return kv;
   }
 
   @Override
   public OrderSystem.KeyValue[] getAll() {
-    return new OrderSystem.KeyValue[0];
+    if (keys == null) {
+      int len = resultMap.size() + goodResultMap.size() - 1;
+      OrderSystem.KeyValue[] kvArray = new OrderSystem.KeyValue[len];
+      int count = 0;
+      for (OrderSystem.KeyValue kv : resultMap.values())
+        kvArray[++count] = kv;
+      goodResultMap.remove("goodid");
+      for (OrderSystem.KeyValue kv : goodResultMap.values())
+        kvArray[++count] = kv;
+      return kvArray;
+    }
+
+    OrderSystem.KeyValue[] kvArray = new OrderSystem.KeyValue[keys.size()];
+    int count = 0;
+    for (OrderSystem.KeyValue kv : resultMap.values())
+      if (!kv.key().equals("orderid") || keys.contains("orderid"))
+        kvArray[++count] = kv;
+    goodResultMap.remove("goodid");
+    for (OrderSystem.KeyValue kv : goodResultMap.values())
+      kvArray[++count] = kv;
+    return kvArray;
   }
 
   @Override
   public long orderId() {
-    return 0;
+    try {
+      return resultMap.get("orderid").valueAsLong();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return 0;
+    }
   }
 
   @Override
   protected boolean needKey(byte[] key, int keyLen) {
-    return keys.contains(new String(key, 0, keyLen));
+    return keys == null ||
+        keys.contains(new String(key, 0, keyLen)) ||
+        new String(key, 0, keyLen).equals("orderid");
   }
 }
